7321b277163dba876dae89aa11e3468fbe03cb7a
Ayush Sonawale <75654165+ayushsonawale@users.noreply.github.com>
2024-08-01 23:25:14 +0530

    Time: 10 ms (54.9%), Space: 16.9 MB (27.73%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        int m = matrix[0].size();;
+        int m = matrix[0].size();
         vector<int> row(n, 0);
         vector<int> col(m, 0);
 
