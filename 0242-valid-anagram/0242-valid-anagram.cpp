85c25c8a06b11db6d893482db5efbee86cf90b87
Ayush Sonawale <75654165+ayushsonawale@users.noreply.github.com>
2024-12-23 20:24:21 +0530

    Time: 0 ms (100%), Space: 9.8 MB (43.81%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        unordered_map<char,int> cntS;
+        unordered_map<char,int> cntT;
+        for(int i = 0; i < s.length(); i++){
+            cntS[s[i]]++;
+            cntT[t[i]]++;
+        }
+        if(cntS == cntT) {
+            return true;
+        }
+        return false;
+    }
+};
